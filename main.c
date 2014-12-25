c34e64897e8b467b51aead277727f7a9c08e39b0
Breaker <791628659@qq.com>
2014-12-25 11:17:22 +0800

    pin16 LED 闪烁

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
+/*
+*   	2014年12月25日10:58:18
+*	V1.0 	By Breaker
+*
+*	文件名：main.h
+*	程序的主函数，永远不返回
+*	
+*/
+
 #include "gpio.h"
 
 void main(void)
 {
+	unsigned int tmp = 0; 
+	GPIO_SET_GPFSEL(16,1);	//GPIO16设置为output
 	while(1)
 	{
+		GPIO_SET_GPCLR(16);
+		for(tmp=0; tmp<50000; tmp++)
+		;
+		GPIO_SET_GPCLR(16);
 		
 	}
 }
